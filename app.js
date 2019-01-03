89de4a7eacddab994e157457a17058b7f4ab3690
apple <haokuowulian@sina.com>
2019-01-03 15:41:23 +0800

    张金飞修改我的授权登录2019-01-03 15:41:20

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 App({
+
    globalData:{
     baseUrl:'http://192.168.1.89:8080/LLGY/'
    },
   onLanuch(){
     const {data} = my.getStorageSync({key: 'logs'});
     const logs = data && data.logs ? data.logs : [];
-    // my.setStorageSync({
-    //   key: '', // 缓存数据的key
-    //   data: '', // 要缓存的数据
-    // });
-
+  },
    
-  }
 })
