5be68309e1f4f6a62a6698f79fd8ad77acd00390
apple <haokuowulian@sina.com>
2019-01-24 10:00:00 +0800

    张金飞新增家具，房屋特色 2019-01-24 09:59:58

--- pages/index/housedelivery/housedelivery5/housedelivery5.js
+++ pages/index/housedelivery/housedelivery5/housedelivery5.js
@@ -11,4 +11,18 @@ Page({
       url: '/pages/index/housedelivery/roominfo/roominfo',
     })
   },
+  //前往便利设施
+  goTo1(){
+     my.hideKeyboard();
+     my.navigateTo({
+      url: '/pages/index/housedelivery/housedelivery5-1/housedelivery5-1',
+    })
+  },
+  //前往房屋特色
+  goTo2(){
+    my.hideKeyboard();
+    my.navigateTo({
+      url: '/pages/index/housedelivery/housedelivery5-2/housedelivery5-2',
+    })
+  }
 });
