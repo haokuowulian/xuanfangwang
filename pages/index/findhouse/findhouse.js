0acf29d3af478e1df42ffb672762d8c1d2a684d4
suchenkeji@sina.com <you@example.com>
2019-06-09 09:37:02 +0800

    金建达提交2019-6-9 09:37:00

--- pages/index/findhouse/findhouse.js
+++ pages/index/findhouse/findhouse.js
@@ -1,66 +1,3 @@
-const areaHouse = [
-  {
-    areaCode:'330106',
-    areaName:'西湖区',
-    houseCount:'10',
-    longitude:'120.1302600000',
-    latitude:'30.2596100000',
-  },
-  {
-    areaCode:'330108',
-    areaName:'滨江区',
-    houseCount:'10',
-    longitude:'120.2120100000',
-    latitude:'30.2084000000',
-  },
-  {
-    areaCode:'330109',
-    areaName:'萧山区',
-    houseCount:'10',
-    longitude:'120.2645700000',
-    latitude:'30.1853400000',
-  },
-  {
-    areaCode:'330102',
-    areaName:'上城区',
-    houseCount:'10',
-    longitude:'120.1692900000',
-    latitude:'30.2425900000',
-  },
-];
-
-const streetHouse = [
-  {
-    areaCode:'330102001',
-    areaName:'清波街道',
-    houseCount:'10',
-    longitude:'120.1609400000',
-    latitude:'30.2383400000',
-  },
-  {
-    areaCode:'330108001',
-    areaName:'西兴街道',
-    houseCount:'10',
-    longitude:'120.2329200000',
-    latitude:'30.1871900000',
-  },
-  {
-    areaCode:'330109109',
-    areaName:'闻堰镇',
-    houseCount:'10',
-    longitude:'120.1838000000',
-    latitude:'30.1230700000',
-  },
-  {
-    areaCode:'330106008',
-    areaName:'西湖街道',
-    houseCount:'10',
-    longitude:'120.1250680000',
-    latitude:'30.2253630000',
-  },
-];
-
-
 const app = getApp();
 
 
@@ -69,12 +6,15 @@ const includePoints = [{
   longitude: 120.10675,
 }];
 
+
+
 Page({
   data: {
     scale: 11,
     lng: '',
     lat: '',
     nearByHouseList:[],
+    circles:[],
     markers: '',
     titleCss1: true,
     titleCss2: false,
@@ -123,8 +63,8 @@ Page({
             } ]
         });
        
-        // that.getNearByHousing();
-        that.setMarkers(areaHouse,that.data.scale);
+        that.getNearByHousing();
+        
       },
       fail() {
         my.hideLoading();
@@ -132,21 +72,47 @@ Page({
       },
     })
  },
-
-  setMarkers(houseList,scale){
-    var that = this;
-    var houseMarkers=[];
-    
-
-    for(var i=0;i<houseList.length;i++){
+  //获取附近房源
+  getNearByHousing(){
+     var that=this;
+     my.httpRequest({
+      url: app.globalData.baseUrl_whj+"IF/homePage/getHomeHouseIF.do",
+      method: 'POST',
+      data: {
+        lng: this.data.lng,
+        lat: this.data.lat,
+        distance:this.data.distance
+      },
+      dataType: 'json',
+      success: function(res) {
+        console.log(res.data);
+        if(res.data.success){
+          that.setData({
+            // houseNum:res.data.count,
+            nearByHouseList:res.data.data,
+            housecount:res.data.data.length
+          });
+          that.setMarkers();
+        }
+      },
+      fail: function(res) {
+       console.log(res);
+      },
+      complete: function(res) {
+        my.hideLoading();
+      }
+    });
+  },
+  setMarkers(){
+     var houseMarkers=[];
+    for(var i=0;i<this.data.nearByHouseList.length;i++){
       var obj={
-        id:houseList[i].areaCode,
-        latitude: houseList[i].latitude,
-        longitude: houseList[i].longitude,
+        id:i+1,
+        latitude: this.data.nearByHouseList[i].latitude,
+        longitude: this.data.nearByHouseList[i].longitude,
         width: 1,
         height: 1,
-        // iconPath: "/image/circle.png",
-        // iconAppendStr:houseList[i].areaName+' '+houseList[i].houseCount+'套房源',
+        // iconPath: '/image/mark_bs.png',
         "label": {
           content: this.data.nearByHouseList[i].apartment.apartmentName,
           color: "#ffffff",
@@ -160,75 +126,20 @@ Page({
       }
       houseMarkers.push(obj);
     }
-    this.mapCtx.getCenterLocation({
-      success: res => {
-        console.log(res.longitude);
-        console.log(res.latitude);
-        that.setData({
-          lng: res.longitude,
-          lat: res.latitude,
-          markers: houseMarkers,
-          distance:this.data.distance,
-          scale:scale,
-        });
-      }
-    });
-    // this.setData( {
-    //   markers: houseMarkers,
-    //   distance:this.data.distance,
-    // } );
    
+    this.setData( {
+      markers: houseMarkers,
+      distance:this.data.distance
+    } );
   },
   regionchange ( e )
   {
-    var that = this;
     console.log( 'regionchange', e );
-    var scale = e.scale;
-    // this.mapCtx.getCenterLocation({
-    //   success: res => {
-    //     console.log(res.longitude);
-    //     console.log(res.latitude);
-    //     that.setData({
-    //       lng: res.longitude,
-    //       lat: res.latitude,
-    //     });
-    //   }
-    // });
-    console.log(scale)
-    if(scale<12){
-      that.setMarkers(areaHouse);
-      that.setData({
-        scale:scale,
-      });
-    }
-    if(scale>=12&&scale<14){
-      that.setMarkers(streetHouse,scale);
-      that.setData({
-        scale:scale,
-      });
-    }
-    if(scale>=14&&scale<=16){
-      this.mapCtx.getCenterLocation({
-        success: res => {
-          console.log(res.longitude);
-          console.log(res.latitude);
-          that.setData({
-            lng: res.longitude,
-            lat: res.latitude,
-            scale:scale,
-          });
-        }
-      });
-      // that.setData({
-      //   scale:scale,
-      // });
-    }
   },
   markertap ( e )
   {
     console.log( 'marker tap', e );
   },
-  
   controltap ( e )
   {
     console.log( 'control tap', e );
@@ -237,20 +148,23 @@ Page({
   {
     console.log( 'tap' );
   },
-  //点击查询列表
   callouttap ( e )
   {
-    var that = this;
-    var scale = that.data.scale;
     console.log( 'callout tap', e );
-    var id = e.markerId;
-    console.log('areaCode',id)
   },
   mapMoveToLocation ()
   {
     this.mapCtx.moveToLocation();
   },
 
+  // 跳转房屋查找
+  searchHouse(){
+    console.log('222222222222')
+     my.navigateTo({
+      // url: '/pages/index/renting/renting?type=1',
+      url: '/pages/search/search',
+    });
+  },
 
   houseCountClick(e){
     if(this.data.housemenu){
